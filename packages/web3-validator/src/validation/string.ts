2614ec4da7ed081cde0deb5e3d6597c2b8da2021
Ricardo Geraldes <144315960+rgeraldes24@users.noreply.github.com>
2024-11-23 15:28:22 +0400

    packages/web3-validator/src/validation/string.ts: revert change

--- packages/web3-validator/src/validation/string.ts
+++ packages/web3-validator/src/validation/string.ts
@@ -34,7 +34,7 @@ export const isHexStrict = (hex: ValidInputTypes) =>
  */
 export function isHexString(value: string, length?: number): boolean {
 	if (typeof value !== 'string' || !value.match(/^0x[0-9A-Fa-f]*$/)) return false;
-	
+
 	if (typeof length !== 'undefined' && length > 0 && value.length !== 2 + 2 * length)
 		return false;
 
