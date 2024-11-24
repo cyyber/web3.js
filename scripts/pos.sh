79fe3b373e85de18560820639af48b943a68ff31
Ricardo Geraldes <144315960+rgeraldes24@users.noreply.github.com>
2024-11-24 20:34:38 +0400

    scripts/pos.sh: review file

--- scripts/pos.sh
+++ scripts/pos.sh
@@ -31,12 +31,12 @@ download(){
 
     if [ ! -e "$TMP_FOLDER/go-zond" ]
     then
-        git clone -b feature/z-prefix-address https://github.com/rgeraldes24/go-zond ${TMP_FOLDER}/go-zond
+        git clone https://github.com/cyyber/go-zond ${TMP_FOLDER}/go-zond
     fi
 
 	if [ ! -e "$TMP_FOLDER/qrysm" ]
     then
-        git clone -b feature/z-prefix-address https://github.com/rgeraldes24/qrysm ${TMP_FOLDER}/qrysm
+        git clone https://github.com/cyyber/qrysm ${TMP_FOLDER}/qrysm
     fi
 }
 
