9a9e37f71ecb9d742f00a12b6b343ffa45f81b66
Ricardo Geraldes <144315960+rgeraldes24@users.noreply.github.com>
2025-11-18 10:09:08 +0400

    packages/web3-qrl-accounts/test/unit/jest.config.js: review file

--- packages/web3-qrl-accounts/test/unit/jest.config.js
+++ packages/web3-qrl-accounts/test/unit/jest.config.js
@@ -2,11 +2,7 @@ const base = require('../config/jest.config');
 
 module.exports = {
 	...base,
-	// testMatch: ['<rootDir>/test/unit/**/*.(spec|test).(js|ts)'],
-	testMatch: [
-		'<rootDir>/test/unit/**/eip1559.test.ts',
-		// '<rootDir>/test/unit/**/base.test.ts',
-	],
+	testMatch: ['<rootDir>/test/unit/**/*.(spec|test).(js|ts)'],
 
 	coverageDirectory: '../../.coverage/unit',
 	collectCoverageFrom: ['src/**/*.ts'],
